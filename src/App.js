97315a1acb2cbfd596d4ab150bff45d7978d0ce7
KhushiDiwan16 <diwankhushi1234@gmail.com>
2024-09-30 11:43:52 +0530

    added logo

--- src/App.js
+++ src/App.js
@@ -5,15 +5,7 @@ import CourseCategory from './Components/CourseCategory';
 
 import Nav from './Components/Nav';
 import Home from './Components/Home';
-<<<<<<< HEAD
-// import Nav from './Components/Nav';
-// import Nav1 from './Components/Nav1';
-=======
-import Nav from './Components/Nav';
-import Nav1 from './Components/Nav1';
-import Login from './Components/Login';
-import SignUp from './Components/SignUp';
->>>>>>> 7bb31a3c674b9cdab665e4d61e0bb389357e2078
+
 
 
 function App() {

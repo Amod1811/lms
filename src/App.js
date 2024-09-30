293ddbb497df03a02f2663830b08fab66ba37039
tijareshubhangi <shubhangitijare01@gmail.com>
2024-09-30 11:44:53 +0530

    modify

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-
 import CourseCategory from './Components/CourseCategory';
-
 import Nav from './Components/Nav';
 import Home from './Components/Home';
-import Nav from './Components/Nav';
-import Nav1 from './Components/Nav1';
-import Login from './Components/Login';
-import SignUp from './Components/SignUp';
+
+
+
 
 
 function App() {

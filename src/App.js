825b0e8bf74b032c8cf0ca18a437e7af5f06c9df
KhushiDiwan16 <diwankhushi1234@gmail.com>
2024-10-01 12:52:44 +0530

    added Instructor Files

--- src/App.js
+++ src/App.js
@@ -11,6 +11,22 @@ import Login from './Components/Login';
 import SignUp from './Components/SignUp';
 import Footer from './Components/Footer';
 import Nav1 from './Components/Nav1';
+import Instructor_Dashboard from './Components/Instructor_Dashboard';
+import Instructor_Courses from './Components/Instructor_Courses';
+import Instructor_Quiz from './Components/Instructor_Quiz';
+import Instructor_earning from './Components/Instructor_earning';
+import Instructor_studentlist from './Components/Instructor_studentlist';
+import Instructor_order from './Components/Instructor_order';
+import Instructor_Reviews from './Components/Instructor_Reviews';
+import Instructor_Editprofile from './Components/Instructor_Editprofile';
+import Instructor_PayOuts from './Components/Instructor_PayOuts';
+import Instructor_setting from './Components/Instructor_setting';
+import Instructor_Deleteprofile from './Components/Instructor_Deleteprofile';
+import Sign_in from './Components/Sign_in';
+import Sign_UP from './Components/Sign_UP';
+import Instructor_createCourse from './Components/Instructor_createCourse';
+import Instructor_Single from './Components/Instructor_Single';
+import Instructor_List from './Components/Instructor_List';
 
 
 
@@ -20,6 +36,22 @@ function App() {
 
     <Router>
        <Nav/>
+       <Instructor_Dashboard/>
+       <Instructor_Courses/>
+       <Instructor_Quiz/>
+       <Instructor_earning/>
+       <Instructor_studentlist/>
+       <Instructor_order/>
+       <Instructor_Reviews/>
+       <Instructor_Editprofile/>
+       <Instructor_PayOuts/>
+       <Instructor_setting/>
+       <Instructor_Deleteprofile/>
+       <Sign_in/>
+       <Sign_UP/>
+       <Instructor_createCourse/>
+       <Instructor_Single/>
+       <Instructor_List/>
         {/* <Nav1/> */}
           <Routes>
               <Route path='/' element={ <Home/> }/>

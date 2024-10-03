a3896d8b11fbf8cd95fb289f5fc448499b2827ac
KhushiDiwan16 <diwankhushi1234@gmail.com>
2024-10-03 11:11:28 +0530

    added css in logo

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
 import { Link } from 'react-router-dom';
 
 const Nav = () => {
@@ -22,6 +23,7 @@ const Nav = () => {
           <span className="navbar-toggler-animation">
             <span />
             <span />
+            
             <span />
           </span>
         </button>
